e353b6d57abd0ce7b8142cb723b8e876c7d33935
jokamjohn <johnkagga@gmail.com>
2015-07-17 18:59:28 +0300

    dynamically inserting elements in the document

--- AccessingDOM/script.js
+++ AccessingDOM/script.js
@@ -15,3 +15,25 @@ textDiv.setAttribute("align", "right");
 var sideBar = document.getElementById("sidebar");
 console.log("Inner content of the div: ", sideBar.innerHTML);
 
+//creating and adding elements dynamically to the document
+var heading = document.createElement("h1");
+var paragraghy = document.createElement("p");
+
+//adding text to the created tags.....not a good way
+//heading.innerHTML = "Did you know?";
+//paragraghy.innerHTML = "My name is kagga john";
+
+//better way of adding text is using the text node
+var headingText = document.createTextNode("Did you know ?");
+var paragrahyText = document.createTextNode("My name is John Kagga");
+
+//adding the text to the elements
+heading.appendChild(headingText);
+paragraghy.appendChild(paragrahyText);
+
+//attaching the tags to the document
+var trivia = document.getElementById("trivia");
+trivia.appendChild(heading);
+trivia.appendChild(paragraghy);
+
+//look at insertbefore function to create an element before the other
