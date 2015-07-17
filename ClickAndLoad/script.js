60b2e49a726753e75033b4a193663a27334c899e
jokamjohn <johnkagga@gmail.com>
2015-07-17 20:27:41 +0300

    onclick

---
+++ ClickAndLoad/script.js
@@ -0,0 +1,22 @@
+// use the commented-out code for reference, or just write your own.
+
+var image = document.getElementById("mainImage");
+image.onclick = function () {
+    alert("You clicked an image");
+};
+
+//document.onclick = function() {
+//	alert("You clicked somewhere in the document");
+//};
+
+//function prepareEventHandlers() {	
+//	var myImage = document.getElementById("mainImage");
+//	myImage.onclick =  function() {
+//		alert("You clicked the image");
+//	}
+//}
+
+//window.onload = function() {
+// prep anything we need to
+//	prepareEventHandlers();
+//};
