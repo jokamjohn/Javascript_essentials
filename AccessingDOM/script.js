299e6d9a27eedf7767590178700785874aac1910
jokamjohn <johnkagga@gmail.com>
2015-07-17 18:09:21 +0300

    DOM

---
+++ AccessingDOM/script.js
@@ -0,0 +1,11 @@
+var mainTitle = document.getElementById("mainTitle");
+
+console.log("The node is: ", mainTitle.nodeType);
+console.log("The inner html: ", mainTitle.innerHTML);
+console.log("The child node: ", mainTitle.childNodes.length);
+
+var links = document.getElementsByTagName("a");
+console.log("My links: ", links);
+console.log("My links: ", links.length);
+
+
